24dfa45a8371640173c76f96aaed34aad867ea73
nqhtqn <97612424+nqhtqn@users.noreply.github.com>
2022-12-07 09:52:03 +0100

    Update DB.java

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/DB.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/DB.java
@@ -22,5 +22,6 @@ public class DB {
         return con;
     }
     
-    public 
+    
+    
 }
--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,18 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
+//Libraries
 package com.mycompany.mavenproject1;
 import javax.swing.*;
-
-import static com.mycompany.mavenproject1.DB.*;
 import java.sql.*;
+import static com.mycompany.mavenproject1.DB;
 
-/**
- *
- * @author leotr
- */
+//Main
 public class Mavenproject1 {
 
     public static void main(String[] args) {
