b8cf57dcd03e15c1f668c66c8c093ee98e0b00a6
nqhtqn <97612424+nqhtqn@users.noreply.github.com>
2022-12-09 15:32:54 +0100

    Update Manage.java

--- mavenproject1/src/main/java/com/mycompany/javashop/Manage.java
+++ mavenproject1/src/main/java/com/mycompany/javashop/Manage.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 
 //Management
 public class Manage {
-    
+
     public static ArrayList<Product> ListProd(Connection con) throws SQLException {
         System.out.println("test");
         ArrayList<Product> listProd = new ArrayList<Product>();
-        String sql;       
+        String sql;
         Statement stmt = con.createStatement();
         sql = "SELECT * FROM product";
         ResultSet rs = stmt.executeQuery(sql);
@@ -24,5 +24,16 @@ public class Manage {
             listProd.add(prod);
         }
         return listProd;
+    }
+
+    public static boolean isAvailable(Connection con, int idP, int quantity) throws SQLException {
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT storageP FROM product WHERE idP = '" + idP + "'");
+        while (rs.next()) {
+            if (quantity <= rs.getInt("storageP")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
