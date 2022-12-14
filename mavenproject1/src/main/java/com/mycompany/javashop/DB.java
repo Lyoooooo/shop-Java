2ca6156497829cb6a2d1a5d5095a3183533e261b
nqhtqn <97612424+nqhtqn@users.noreply.github.com>
2022-12-14 16:38:18 +0100

    Update DB.java

--- mavenproject1/src/main/java/com/mycompany/javashop/DB.java
+++ mavenproject1/src/main/java/com/mycompany/javashop/DB.java
@@ -10,10 +10,6 @@ public class DB {
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop-java?serverTimezone=UTC","root","");
             System.out.println("Connection is Successful to the database");
-            String query = "Insert into user(idU,nameU,surnameU,mailU,pwU) values(null,'ram','test','test2','test3')";
-            Statement statement = con.createStatement();
-            statement.execute(query);
-
         }
         catch(SQLException e) {
             System.out.println(e);
